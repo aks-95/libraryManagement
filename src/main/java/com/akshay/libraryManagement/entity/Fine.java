421b4e5f22ff7b57e09faa989837fab0fd3f4d51
aks-95 <akshay.km.sharma@gmail.com>
2025-08-24 16:31:51 +0530

    some more method

--- src/main/java/com/akshay/libraryManagement/entity/Fine.java
+++ src/main/java/com/akshay/libraryManagement/entity/Fine.java
@@ -1,4 +1,15 @@
 package com.akshay.libraryManagement.entity;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
 public class Fine {
+    int id;
+    int userId;
+    int bookItemId;
+    double amount;
 }
--- src/main/java/com/akshay/libraryManagement/entity/Library.java
+++ src/main/java/com/akshay/libraryManagement/entity/Library.java
@@ -1,4 +1,9 @@
 package com.akshay.libraryManagement.entity;
 
+import java.util.List;
+
 public class Library {
+    List<Book> books;
+    List<BookItem> bookItems;
+//    List<User> users;
 }
